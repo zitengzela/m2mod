eebb5a695fb16b425e7c74ef296685f617852e46
suncurio suncurio <suncurio@gmail.com>
2020-02-07 16:04:39 +0300

    bump version

--- M2Mod/Version.cs
+++ M2Mod/Version.cs
@@ -4,7 +4,7 @@ namespace M2Mod
     static class Version
     {
         public static readonly int Major = 8;
-        public static readonly int Minor = 2;
-        public static readonly int Patch = 4;
+        public static readonly int Minor = 3;
+        public static readonly int Patch = 0;
     }
 }
